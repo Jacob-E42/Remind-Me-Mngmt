eaecb4583ff30a0aa81e6bfb620501db25fe735d
Jacob Eiferman <jacobeiferman@gmail.com>
2022-11-22 06:53:43 -0500

    added pop-up when delete button is pressed for a task

--- Capstone-Project-1/static/js/main.js
+++ Capstone-Project-1/static/js/main.js
@@ -109,40 +109,15 @@ $editTaskButton.on("click", async function (evt) {
 	window.location.replace(`${BASE_URL}${resp.data}`);
 });
 
-// $createTaskButton.on("click", async function (evt) {
-// 	evt.preventDefault();
-// 	console.debug("createTaskButton");
-
-// 	let csrf_token = $("#csrf_token").val();
-// 	let title = $("#title").val();
-// 	let description = $("#description").val();
-// 	let resp_type = $("#resp_type").val();
-// 	let due_time = $("#due_time").val();
-// 	let is_completed = $("#is_completed").val();
-
-// 	let task = {
-// 		csrf_token: csrf_token,
-// 		title: title,
-// 		description: description,
-// 		resp_type: resp_type,
-// 		due_time: due_time,
-// 		is_completed: is_completed
-// 	};
-
-// 	const resp = await axios.post(`${BASE_URL}/tasks`, task);
-
-// 	window.location.replace(`${BASE_URL}${resp.data}`);
-// });
-
 $deleteTaskButton.on("click", async function (evt) {
 	evt.preventDefault();
 	console.debug("DeleteTaskButton: onClick");
 	const target = $(evt.target);
 	const task_id = target.data("task-id");
-
+	console.log(task_id);
 	const resp = await axios.delete(`${BASE_URL}/tasks/${task_id}`);
 
-	window.location.replace(`${BASE_URL}/${resp.data}`);
+	window.location.replace(`${BASE_URL}${resp.data}`);
 });
 
 $("input[type=checkbox]").ready(async function () {
